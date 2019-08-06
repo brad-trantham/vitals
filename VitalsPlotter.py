b2bb9be9223fed828c5031f2025dcf7ccea6938a
Brad Trantham <trantham@gmail.com>
2019-08-05 21:08:21 -0500

    Added limits for all plots

--- VitalsPlotter.py
+++ VitalsPlotter.py
@@ -10,8 +10,12 @@ def extend_limit_list(limit, year):
 file = '/Users/Brad/Vitals-Sheet1.csv'
 
 limits = {}
-limits['Weight'] = [200,240]
+limits['Weight'] = [205,246]
 limits['Total Cholesterol'] = [200,240]
+limits['Triglycerides'] = [150, 200]
+limits['LDL'] = [130, 160]
+limits['VLDL'] = [30]
+limits['HDL'] = [45]
 
 year = []
 weight = []
@@ -38,31 +42,43 @@ plt.plot(year, extend_limit_list(limits['Weight'][0],year), 'y')
 plt.plot(year, extend_limit_list(limits['Weight'][1], year), 'r')
 plt.locator_params(axis='x', nbins=5)
 plt.ylabel('Weight')
+plt.xlabel('Year')
 plt.show()
 
 plt.plot(year,total_chl)
 plt.plot(year, extend_limit_list(limits['Total Cholesterol'][0],year), 'y')
 plt.plot(year, extend_limit_list(limits['Total Cholesterol'][1], year), 'r')
 plt.ylabel('Total Cholesterol')
+plt.xlabel('Year')
 plt.locator_params(axis='x', nbins=5)
 plt.show()
 
 plt.plot(year,trig)
+plt.plot(year, extend_limit_list(limits['Triglycerides'][0],year), 'y')
+plt.plot(year, extend_limit_list(limits['Triglycerides'][1], year), 'r')
 plt.ylabel('Triglycerides')
+plt.xlabel('Year')
 plt.locator_params(axis='x', nbins=5)
 plt.show()
 
 plt.plot(year,hdl)
 plt.ylabel('HDL')
+plt.xlabel('Year')
+plt.plot(year, extend_limit_list(limits['HDL'][0],year), 'g')
 plt.locator_params(axis='x', nbins=5)
 plt.show()
 
 plt.plot(year,ldl)
 plt.ylabel('LDL')
+plt.xlabel('Year')
+plt.plot(year, extend_limit_list(limits['LDL'][0],year), 'y')
+plt.plot(year, extend_limit_list(limits['LDL'][1], year), 'r')
 plt.locator_params(axis='x', nbins=5)
 plt.show()
 
 plt.plot(year,vldl)
+plt.plot(year, extend_limit_list(limits['VLDL'][0],year), 'y')
 plt.ylabel('VLDL')
+plt.xlabel('Year')
 plt.locator_params(axis='x', nbins=5)
 plt.show()
